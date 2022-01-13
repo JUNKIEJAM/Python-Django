1966b15cf63223753fca61e4e2b05ee2ab14c2e5
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-14 00:04:41 +0530

    Login & SignUp

---
+++ login_signup.py
@@ -0,0 +1,20 @@
+print('Create your Account')
+
+username=input('Enter username: ')
+password=input('Enter password: ')
+
+print('You Account has been created Successfully')
+print('Go to Login Page')
+
+username2=input('Enter UserName: ')
+password2=input('Enter Password: ')
+
+if username==username2 and password==password2:
+    print('Login Successfully')
+    
+else:
+    print('Invalid Credentials')
+
+
+
+
